a6877a5a92ba6962da697172be2ac41a11f5c055
KolibSoft <kolibsoftware@gmail.com>
2023-09-15 15:57:43 -0600

    SFML lib added

---
+++ tst/sfml.cpp
@@ -0,0 +1,25 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode({640, 468}), "SFML Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::White);
+        window.display();
+    }
+
+    return 0;
+}
