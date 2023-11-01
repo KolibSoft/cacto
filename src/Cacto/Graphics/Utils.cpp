9882cc30c68e4e588d4f8878850d253323138f65
KolibSoft <kolibsoftware@gmail.com>
2023-11-01 16:43:11 -0600

    fitSize utils function added

--- src/Cacto/Graphics/Utils.cpp
+++ src/Cacto/Graphics/Utils.cpp
@@ -118,4 +118,26 @@ namespace cacto
         return result;
     }
 
+    sf::Vector2f fitSize(const sf::Vector2f &contentSize, const sf::Vector2f &containerSize)
+    {
+        sf::Vector2f size{0, 0};
+        auto hRatio = contentSize.x / containerSize.x;
+        auto vRatio = contentSize.y / containerSize.y;
+        if (hRatio < vRatio)
+        {
+            size.y = containerSize.y;
+            size.x = containerSize.y * contentSize.x / contentSize.y;
+        }
+        else if (vRatio < hRatio)
+        {
+            size.x = containerSize.x;
+            size.y = containerSize.x * contentSize.y / contentSize.x;
+        }
+        else
+        {
+            size = containerSize;
+        }
+        return size;
+    }
+
 }
---
+++ tst/Cacto/Graphics/utils-test.cpp
@@ -0,0 +1,47 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+#include <Cacto/Graphics/Utils.hpp>
+#include <Cacto/Graphics/Rectangle.hpp>
+
+auto makeSquare(const sf::Color &color, const sf::FloatRect &rect)
+{
+    sf::VertexArray array(sf::PrimitiveType::TriangleFan);
+    cacto::setPoints(array, cacto::Rectangle::Identity);
+    cacto::setColor(array, color);
+    cacto::mapPositions(array, rect);
+    return array;
+}
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode({640, 468}), "SFML Window");
+    auto container = makeSquare(sf::Color::Red, {{10, 10}, {300, 200}});
+    auto content = makeSquare(sf::Color::Blue, {{10, 10}, {100, 200}});
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        cacto::mapPositions(container, {{10, 10}, sf::Vector2f(sf::Mouse::getPosition(window))});
+
+        auto scale = cacto::fitSize({content.getBounds().width, content.getBounds().height}, {container.getBounds().width, container.getBounds().height});
+        cacto::mapPositions(content, {{10, 10}, scale});
+
+        window.clear();
+        window.draw(container);
+        window.draw(content);
+        window.display();
+    }
+
+    return 0;
+}
