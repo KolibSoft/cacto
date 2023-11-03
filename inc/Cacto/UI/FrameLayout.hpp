d622c33f455e31a5ffc2a4a26894bf6fed95958d
KolibSoft <kolibsoftware@gmail.com>
2023-11-03 13:55:37 -0600

    Frame layout reimplemented

---
+++ inc/Cacto/UI/FrameLayout.hpp
@@ -0,0 +1,52 @@
+#ifndef CACTO_FRAME_LAYOUT_HPP
+#define CACTO_FRAME_LAYOUT_HPP
+
+#include <Cacto/UI/Span.hpp>
+#include <Cacto/UI/Block.hpp>
+
+namespace cacto
+{
+
+    class CACTO_UI_API FrameLayout
+        : public Block
+    {
+
+    public:
+        szt getChildCount() const override;
+        Node *const getChild(szt index = 0) const override;
+
+        Anchor getHorizontalAnchor() const;
+        void setHorizontalAnchor(Anchor value);
+
+        Anchor getVerticalAnchor() const;
+        void setVerticalAnchor(Anchor value);
+
+        void append(Node &child);
+        void remove(Node &child);
+
+        FrameLayout();
+        virtual ~FrameLayout();
+
+        FrameLayout(const FrameLayout &other);
+        FrameLayout &operator=(const FrameLayout &other);
+
+    protected:
+        void onAppend(Node &child) override;
+        void onRemove(Node &child) override;
+
+        void onDraw(sf::RenderTarget &target, const sf::RenderStates &states) const override;
+
+        sf::Vector2f onCompact() override;
+        sf::Vector2f onInflate(const sf::Vector2f &containerSize) override;
+        void onPlace(const sf::Vector2f &position) override;
+
+    private:
+        Node *m_child;
+        Anchor m_hAnchor;
+        Anchor m_vAnchor;
+        sf::Vector2f m_childSize;
+    };
+
+}
+
+#endif
