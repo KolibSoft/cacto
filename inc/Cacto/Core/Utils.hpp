e86a826855d479adcdffb6f4b443f646c305c9c3
KolibSoft <kolibsoftware@gmail.com>
2023-11-21 20:01:08 -0600

    StringMap removed

--- inc/Cacto/Core/Utils.hpp
+++ inc/Cacto/Core/Utils.hpp
@@ -1,19 +1,10 @@
 #ifndef CACTO_CORE_UTILS_HPP
 #define CACTO_CORE_UTILS_HPP
 
-#include <Cacto/Lang/JsonValue.hpp>
 #include <Cacto/Core/Export.hpp>
 
 namespace cacto
 {
-
-    using StringMap = std::unordered_map<std::string, std::string>;
-
-    JsonValue CACTO_CORE_API stringMapToJson(const StringMap &map);
-    void CACTO_CORE_API stringMapFromJson(StringMap &map, const JsonValue &json);
-
-    void CACTO_CORE_API loadStrings(const std::filesystem::path &path = "res/strings.json");
-    extern StringMap CACTO_CORE_API Strings;
 
 }
 
--- src/Cacto/Core/Utils.cpp
+++ src/Cacto/Core/Utils.cpp
@@ -3,28 +3,4 @@
 namespace cacto
 {
 
-    JsonValue stringMapToJson(const StringMap &map)
-    {
-        auto json = JsonValue::ObjectValue;
-        for (auto &pair : map)
-            json[pair.first] = pair.second;
-        return json;
-    }
-
-    void stringMapFromJson(StringMap &map, const JsonValue &json)
-    {
-        map.clear();
-        for (auto &pair : json.asObject())
-            map[pair.first] = pair.second.asString();
-    }
-
-    void loadStrings(const std::filesystem::path &path)
-    {
-        JsonValue json = nullptr;
-        json.fromFile(path);
-        stringMapFromJson(Strings, json);
-    }
-
-    StringMap Strings{};
-
 }
