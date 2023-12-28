15fab82239956d18237a908cfd67521421ea0147
KolibSoft <kolibsoftware@gmail.com>
2023-12-28 00:18:25 -0600

    Checkpoint

--- src/Cacto/UI/FrameLayout.cpp
+++ src/Cacto/UI/FrameLayout.cpp
@@ -5,18 +5,6 @@
 namespace cacto
 {
 
-    szt FrameLayout::getChildCount() const
-    {
-        return m_holder != nullptr ? 1 : 0;
-    }
-
-    Node *const FrameLayout::getChild(szt index) const
-    {
-        if (index >= 1 || !m_holder)
-            return nullptr;
-        return &m_holder->getNode();
-    }
-
     FrameLayout::Anchor FrameLayout::getHorizontalAnchor() const
     {
         return m_hAnchor;
@@ -39,107 +27,96 @@ namespace cacto
         return *this;
     }
 
-    void FrameLayout::append(Node &child)
+    ParentNode *const FrameLayout::getParent() const
     {
-        Node::link(*this, child);
+        return Block::getParent();
     }
 
-    void FrameLayout::remove(Node &child)
+    szt FrameLayout::getChildCount() const
     {
-        Node::unlink(*this, child);
+        return m_child != nullptr ? 1 : 0;
+    }
+
+    ChildNode *const FrameLayout::getChild(szt index) const
+    {
+        if (index >= 1)
+            return nullptr;
+        return m_child;
+    }
+
+    void FrameLayout::append(ChildNode &child)
+    {
+        if (m_child == &child)
+            return;
+        if (m_child != nullptr)
+            throw std::runtime_error("This node has another child already");
+        if (child.getParent() != nullptr && child.getParent() != this)
+            throw std::runtime_error("The node is already attached to another parent");
+        if (hasAncestor(child))
+            throw std::runtime_error("The node is an ancestor");
+        m_child = &child;
+        child.attach(*this);
+    }
+
+    void FrameLayout::remove(ChildNode &child)
+    {
+        if (m_child == &child)
+        {
+            m_child = nullptr;
+            child.detach();
+        }
+    }
+
+    void FrameLayout::draw(sf::RenderTarget &target, const sf::RenderStates &states) const
+    {
+        drawBlock(target, states);
+        if (m_child)
+            DrawNode::draw(*m_child, target, states);
+    }
+
+    sf::Vector2f FrameLayout::compact()
+    {
+        auto contentSize = m_child ? InflatableNode::compact(*m_child) : sf::Vector2f{0, 0};
+        auto size = compactBlock(contentSize);
+        return size;
+    }
+
+    sf::Vector2f FrameLayout::inflate(const sf::Vector2f &containerSize)
+    {
+        auto size = inflateBlock(containerSize);
+        if (m_child)
+        {
+            auto contentBox = getContentBox();
+            m_childSize = InflatableNode::inflate(*m_child, {contentBox.getWidth(), contentBox.getHeight()});
+        }
+        return size;
+    }
+
+    void FrameLayout::place(const sf::Vector2f &position)
+    {
+        placeBlock(position);
+        if (m_child)
+        {
+            auto contentBox = getContentBox();
+            contentBox.setWidth(m_childSize.x, m_hAnchor);
+            contentBox.setHeight(m_childSize.y, m_vAnchor);
+            InflatableNode::place(*m_child, {contentBox.getLeft(), contentBox.getTop()});
+        }
     }
 
     FrameLayout::FrameLayout()
         : Block(),
           m_hAnchor(Start),
           m_vAnchor(Start),
-          m_holder()
+          m_childSize(),
+          m_child()
     {
     }
 
     FrameLayout::~FrameLayout()
     {
-        if (m_holder)
-            Node::unlink(*this, m_holder->getNode());
-    }
-
-    FrameLayout::FrameLayout(const FrameLayout &other)
-        : Block(other),
-          m_hAnchor(other.m_hAnchor),
-          m_vAnchor(other.m_vAnchor),
-          m_holder(nullptr)
-    {
-    }
-
-    FrameLayout &FrameLayout::operator=(const FrameLayout &other)
-    {
-        Block::operator=(other);
-        m_hAnchor = other.m_hAnchor;
-        m_vAnchor = other.m_vAnchor;
-        return *this;
-    }
-
-    const FrameLayout::Holder *const FrameLayout::getHolder() const
-    {
-        return m_holder;
-    }
-
-    FrameLayout::Holder *const FrameLayout::getHolder()
-    {
-        return m_holder;
-    }
-
-    void FrameLayout::onAppend(Node &child)
-    {
-        if (m_holder != nullptr)
-            throw std::runtime_error("This node has another child already");
-        m_holder = new Holder(child);
-    }
-
-    void FrameLayout::onRemove(Node &child)
-    {
-        if (!m_holder || &m_holder->getNode() != &child)
-            throw std::runtime_error("This node has another child already");
-        delete m_holder;
-        m_holder = nullptr;
-    }
-
-    void FrameLayout::onDraw(sf::RenderTarget &target, const sf::RenderStates &states) const
-    {
-        drawBlock(target, states);
-        if (m_holder)
-            DrawNode::draw(m_holder->getNode(), target, states);
-    }
-
-    sf::Vector2f FrameLayout::onCompact()
-    {
-        auto contentSize = m_holder ? m_holder->compact() : sf::Vector2f{0, 0};
-        auto size = compactBlock(contentSize);
-        return size;
-    }
-
-    sf::Vector2f FrameLayout::onInflate(const sf::Vector2f &containerSize)
-    {
-        auto size = inflateBlock(containerSize);
-        if (m_holder)
-        {
-            auto contentBox = getContentBox();
-            m_holder->inflate({contentBox.getWidth(), contentBox.getHeight()});
-        }
-        return size;
-    }
-
-    void FrameLayout::onPlace(const sf::Vector2f &position)
-    {
-        placeBlock(position);
-        if (m_holder)
-        {
-            auto contentBox = getContentBox();
-            auto &childBox = m_holder->getBox();
-            contentBox.setWidth(childBox.getWidth(), m_hAnchor);
-            contentBox.setHeight(childBox.getHeight(), m_vAnchor);
-            m_holder->place({contentBox.getLeft(), contentBox.getTop()});
-        }
+        if (m_child)
+            remove(*m_child);
     }
 
 }
