93c1a5372764ab5c5a0854f3deb5e7f0ca2a8f3c
KolibSoft <kolibsoftware@gmail.com>
2024-01-29 16:34:12 -0600

    Vertex Array Xml test

---
+++ tst/Cacto/Graphics/vertex-array-xml-test.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <fstream>
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+#include <Cacto/Lang/XmlValue.hpp>
+#include <Cacto/Graphics/VertexArrayUtils.hpp>
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode({640, 468}), "SFML Window");
+    sf::VertexArray array{};
+    cacto::XmlValue xml{};
+
+    std::ifstream istream{"res/vertex_array.xml"};
+    istream >> xml;
+    array = cacto::toVertexArray(xml);
+
+    std::ofstream ostream{"res/vertex_array.xml"};
+    xml = cacto::toXml(array);
+    ostream << xml;
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(array);
+        window.display();
+    }
+
+    return 0;
+}
