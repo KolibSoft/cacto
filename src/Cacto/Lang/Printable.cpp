ccb364b6e5cd821a9decaf1d991faa225f629abb
KolibSoft <kolibsoftware@gmail.com>
2024-01-31 12:59:15 -0600

    Printable/Scannable utils methods added

--- src/Cacto/Lang/Printable.cpp
+++ src/Cacto/Lang/Printable.cpp
@@ -1,8 +1,32 @@
+#include <sstream>
+#include <fstream>
 #include <Cacto/Lang/Printer.hpp>
 #include <Cacto/Lang/Printable.hpp>
 
 namespace cacto
 {
+
+    std::string Printable::toString() const
+    {
+        std::stringstream stream{};
+        stream << *this;
+        auto string = stream.str();
+        return std::move(string);
+    }
+
+    bool Printable::toFile(const std::filesystem::path &path) const
+    {
+        try
+        {
+            std::ofstream stream{path};
+            stream << *this;
+            return true;
+        }
+        catch (...)
+        {
+            return false;
+        }
+    }
 
     std::ostream &operator<<(std::ostream &stream, const Printable &printable)
     {
